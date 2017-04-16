8488e82486c83ab04f234f1e33e1420f76663cd1
Tomasz Chadzynski <t.chadzynski@gmail.com>
2017-04-16 00:26:03 -0700

    Counting pt hits rather than tlb misses for tlb miss and page table hit portion of the time

--- VirtualMemorySimulation/src/Timings.cpp
+++ VirtualMemorySimulation/src/Timings.cpp
@@ -14,17 +14,17 @@ uint32_t whenTlbHits(uint32_t tlbHits)
 // Time it takes for MM to get page if page is found in page table
 // Algorithm: (TLB search time + 2 * main memory access time) *
 //				times page is not found in TLB
-uint32_t whenTlbMisses(uint32_t tlbMisses, uint32_t ptHits)
+uint32_t whenTlbMisses(uint32_t ptHits)
 {
 	uint32_t result;
 
-	result = (TLB_SEARCH + 2*MM_ACCESS) * tlbMisses;
+	result = (TLB_SEARCH + 2*MM_ACCESS) * ptHits;
 
 	return result;
 }
 
 ///Time it takes for MM to get page if there was a page fault
-uint32_t whenPageFault(uint32_t tlbMisses, uint32_t ptMisses)
+uint32_t whenPageFault(uint32_t ptMisses)
 {
 	uint32_t result;
 
@@ -38,8 +38,8 @@ float totalTime(VM::Result &result)
 	float totalTime;
 
 	totalTime = whenTlbHits(result.getTLBStats().hitCount) +
-			 whenTlbMisses(result.getTLBStats().missCount, result.getPtStats().hitCount) +
-			 whenPageFault(result.getTLBStats().missCount, result.getPtStats().missCount);
+			 whenTlbMisses(result.getPtStats().hitCount) +
+			 whenPageFault(result.getPtStats().missCount);
 
 	return totalTime;
 }
