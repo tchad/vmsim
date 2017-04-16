a4796fb47f5c797091c6fe9aca9490d5164d8cb1
Tomasz Chadzynski <t.chadzynski@gmail.com>
2017-04-16 02:44:49 -0700

    Changed timings to correspond to the values given in book

--- VirtualMemorySimulation/src/Timings.h
+++ VirtualMemorySimulation/src/Timings.h
@@ -6,9 +6,9 @@
 #include "VM.h"
 
 //All units are in nanoseconds
-constexpr uint32_t PAGE_REPLACE_COST = 100000; //100us
-constexpr uint32_t TLB_SEARCH = 100; //100ns
-constexpr uint32_t MM_ACCESS = 1000; //1us
+constexpr uint32_t PAGE_REPLACE_COST = 8000000; //8ms
+constexpr uint32_t TLB_SEARCH = 20; //20ns
+constexpr uint32_t MM_ACCESS = 100; //100ns
 
 uint32_t whenTlbHits(uint32_t tlbHits);
 uint32_t whenTlbMisses(uint32_t tlbMisses, uint32_t ptHits);
